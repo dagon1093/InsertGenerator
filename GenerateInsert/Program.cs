791ea0ada231e8151c573ead57ba6eb5b66b36d0
dagon1093 <dagon1093@gmail.com>
2023-11-04 22:20:09 +0300

    Получение столбцов вынесено в отдельный метод

--- GenerateInsert/Program.cs
+++ GenerateInsert/Program.cs
@@ -1,60 +1,65 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 
+using GenerateInsert;
 using Npgsql;
 using System.Text;
 
 
 
 Console.WriteLine("Insert generator");
-var CONNECTION_STRING = "Host=localhost;Username=postgres;Password=Rp_9i7g7;Database=elib_db";
+
+var CONNECTION_STRING = "Host=localhost;Username=postgres;Password= ;Database= ";
 var connection = new NpgsqlConnection(CONNECTION_STRING);
-connection.Open();
-string commandText = "SELECT * FROM Book";
-string cmdColumnNames = "SELECT column_name\r\nFROM information_schema.columns\r\nWHERE table_schema='public' AND table_name='book' ";
-List<string> columnNames = new List<string>();
-
-await using (NpgsqlCommand command = new NpgsqlCommand(cmdColumnNames,connection))
+if (args.Length == 0)
 {
-    await using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
+    Console.WriteLine("Используется для подключения к postgres и генерированию insert запросов по каждой строке данных в таблице");
+    Console.WriteLine("Аргуементы: ");
+    Console.WriteLine("[table] выбор таблицы");
+
+} else {
+
+    string path = "inserts.sql";
+    string commandText = $"SELECT * FROM {args[0]};";
+
+    connection.Open();
+
+    List<string> columnNames = await Utils.GetTableColumnsAsync(connection, args[0]);
+
+
+    await using (NpgsqlCommand command = new NpgsqlCommand(commandText, connection))
     {
-        while (await reader.ReadAsync())
+        await using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
         {
-            columnNames.Add(reader.GetString(0));
-        }
-    }
-    Console.WriteLine($"Columns ({string.Join(", ",columnNames)})");
 
-}
+            int numberOfColumns = reader.FieldCount;
 
-await using (NpgsqlCommand command = new NpgsqlCommand(commandText, connection))
-{
-    await using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
-    {
-
-        int numberOfColumns = reader.FieldCount;
-        
-
-        while (await reader.ReadAsync())
-        {
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < numberOfColumns; i++)
+            while (await reader.ReadAsync())
             {
-                if (reader.GetFieldValue<object>(i) is int a)
-                    sb.Append(a.ToString() + $"{(i != numberOfColumns - 2 ? ", " : "")}");
-                else if (reader.GetFieldValue<object>(i) is string s)
-                    sb.Append($"\'{s}\'" + $"{(i != numberOfColumns - 2 ? ", " : "")}");
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < numberOfColumns; i++)
+                {
+                    if (reader.GetFieldValue<object>(i) is int a)
+                        sb.Append(a.ToString() + $"{(i != numberOfColumns - 2 ? ", " : "")}");
+                    else if (reader.GetFieldValue<object>(i) is string s)
+                        sb.Append($"\'{s}\'" + $"{(i != numberOfColumns - 2 ? ", " : "")}");
+                }
+                string finalString = sb.ToString();
+                string stringToWrite = $"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)});";
+                Utils.writeToFile(path, stringToWrite, true);
+                //ToDo log4net Console.WriteLine($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)})");
+                //DELETE -- await writer.WriteLineAsync($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)});");
+
+
             }
-            string finalString = sb.ToString();
-            Console.WriteLine($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)})");
-            
+
         }
-
     }
-}
 
-connection.Close();
+    connection.Close();
+}
+Console.WriteLine("Нажмите кнопку для выхода: ");
 Console.ReadLine();
 
 
--- GenerateInsert/Utils.cs
+++ GenerateInsert/Utils.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +12,50 @@ namespace GenerateInsert
 {
     static class Utils
     {
+        /// <summary>
+        /// Возвращает список имен столбцов таблицы <param name="tableName">"tableName"</param>.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public static async Task<List<string>> GetTableColumnsAsync(NpgsqlConnection connection, string tableName)
+        {
+            var columns = new List<string>();
+            string cmdColumnNames = $"SELECT column_name\r\nFROM information_schema.columns\r\nWHERE table_schema='public' AND table_name='{tableName}' ";
+
+            await using (NpgsqlCommand command = new NpgsqlCommand(cmdColumnNames, connection))
+            {
+                await using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        columns.Add(reader.GetString(0));
+                    }
+                }
+                //todo log4net Console.WriteLine($"Columns ({string.Join(", ", columns)})");
+                return columns;
+
+            }
+        }
+
+        /// <summary>
+        /// Записывает строку <param name="stringToWrite">"stringToWrite"</param> в файл <param name="fileName">"fileName"</param>
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="stringToWrite"></param>
+        /// <param name="append"></param>
+        public static async void writeToFile(string fileName, string stringToWrite, bool append)
+        {
+
+            using (StreamWriter writer = new StreamWriter(fileName, append))
+            {
+                    await writer.WriteLineAsync(stringToWrite);
+
+            }
+        }
+
+       
         
+
     }
 }
