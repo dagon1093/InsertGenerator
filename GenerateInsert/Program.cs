c66ef014daec46018708cbab1619b38f02e2f5b5
dagon1093 <dagon1093@gmail.com>
2023-11-06 21:23:22 +0300

    Добавлена обработка нескольких таблиц Close#5

--- GenerateInsert/Program.cs
+++ GenerateInsert/Program.cs
@@ -6,7 +6,6 @@ using Npgsql;
 using System.Text;
 
 
-
 Console.WriteLine("Insert generator");
 
 var CONNECTION_STRING = "Host=localhost;Username=postgres;Password=Rp_9i7g7;Database=elib_db";
@@ -15,53 +14,56 @@ if (args.Length == 0)
 {
     Console.WriteLine("Используется для подключения к postgres и генерированию insert запросов по каждой строке данных в таблице");
     Console.WriteLine("Аргуементы: ");
-    Console.WriteLine("[table] выбор таблицы");
+    Console.WriteLine("[table1] [table2] .. [tableN] выбор таблиц");
 
 } else {
 
-    string path = $"{args[0]}_inserts.sql";
-    string commandText = $"SELECT * FROM {args[0]};";
+    for (int arg = 0; arg < args.Length; arg++) { 
+        string tableName = args[arg];
+        string path = $"{tableName}_inserts.sql";
+        string commandText = $"SELECT * FROM {tableName};";
 
-    connection.Open();
+        connection.Open();
 
-    List<string> columnNames = await Utils.GetTableColumnsAsync(connection, args[0]);
+        List<string> columnNames = await Utils.GetTableColumnsAsync(connection, tableName);
 
 
-    await using (NpgsqlCommand command = new NpgsqlCommand(commandText, connection))
-    {
-        await using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
+        await using (NpgsqlCommand command = new NpgsqlCommand(commandText, connection))
         {
-
-            int numberOfColumns = reader.FieldCount;
-
-            while (await reader.ReadAsync())
+            await using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
             {
-                StringBuilder sb = new StringBuilder();
 
-                for (int i = 0; i < numberOfColumns; i++)
+                int numberOfColumns = reader.FieldCount;
+
+                while (await reader.ReadAsync())
                 {
-                    if (reader.GetFieldValue<object>(i) is int a)
-                        sb.Append(a.ToString() + $"{(i != numberOfColumns - 1 ? ", " : "")}");
-                    else if (reader.GetFieldValue<object>(i) is string s)
-                        sb.Append($"\'{s}\'" + $"{(i != numberOfColumns - 1 ? ", " : "")}");
-                    else if (reader.GetFieldValue<object>(i) is DateTime dt)
-                        sb.Append($"\'{dt}\'" + $"{(i != numberOfColumns - 1 ? ", " : "")}");
-                }
-                string finalString = sb.ToString();
-                string stringToWrite = $"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)});";
-                Utils.writeToFile(path, stringToWrite, true);
-                //ToDo log4net or serilog Console.WriteLine($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)})");
-                //DELETE -- await writer.WriteLineAsync($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)});");
+                    StringBuilder sb = new StringBuilder();
 
+                    for (int i = 0; i < numberOfColumns; i++)
+                    {
+                        if (reader.GetFieldValue<object>(i) is int a)
+                            sb.Append(a.ToString() + $"{(i != numberOfColumns - 1 ? ", " : "")}");
+                        else if (reader.GetFieldValue<object>(i) is string s)
+                            sb.Append($"\'{s}\'" + $"{(i != numberOfColumns - 1 ? ", " : "")}");
+                        else if (reader.GetFieldValue<object>(i) is DateTime dt)
+                            sb.Append($"\'{dt}\'" + $"{(i != numberOfColumns - 1 ? ", " : "")}");
+                    }
+                    string finalString = sb.ToString();
+                    string stringToWrite = $"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)});";
+                    Utils.writeToFile(path, stringToWrite, true);
+                    //ToDo log4net or serilog Console.WriteLine($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)})");
+                    //DELETE -- await writer.WriteLineAsync($"INSERT INTO BOOK ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", finalString)});");
+
+                }
 
             }
-
         }
+        connection.Close();
+        
     }
-    connection.Close();
-    connection.Dispose();
-
 }
+
+connection.Dispose();
 Console.WriteLine("Нажмите кнопку для выхода: ");
 Console.ReadLine();
 
